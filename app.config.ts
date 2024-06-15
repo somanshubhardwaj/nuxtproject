9390bcb4e3db5fd5dfd59f02fe9552cdae4866f3
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-06-15 10:23:47 +0530

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,10 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Python Guide',
+    description: 'A guide to your python journey ',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: 'somanshubha',
       github: 'somanshubhardwaj',
@@ -16,28 +17,39 @@ export default defineAppConfig({
 
       instagram: 'https://www.instagram.com/bhardwaj.somanshu/'
     },
+
     github: {
-      dir: '.',
+      dir: '/content/',
       branch: 'main',
       repo: 'nuxtproject',
       owner: 'somanshubhardwaj',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
       fluid: true,
       title: 'A Python Guide'
+    },
+
+    footer: {
+      credits: {
+        text: 'Brought to you by Somanshu Bhardwaj',
+        href: 'https://somanshubhardwaj.vercel.app'
+      }
     }
   }
 })
